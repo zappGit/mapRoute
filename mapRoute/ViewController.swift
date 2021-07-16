40e017826a00e74d4629459481789006a92e9122
Artyom Khrebtov <t.khrebtov@yandex.ru>
2021-07-16 19:51:13 +0500

    Initial Commit

---
+++ mapRoute/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mapRoute
+//
+//  Created by Артем Хребтов on 16.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
